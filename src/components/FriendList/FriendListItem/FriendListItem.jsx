6204df173e84adf5a894e056b9189c2a1d64487f
Anna Sibova <anna_sibova@ukr.net>
2023-04-15 22:45:54 +0300

    fix

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,5 @@
-import css from './FriendListItem.module.css';
 import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
