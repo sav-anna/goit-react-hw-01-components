74c43567285d9f4707039c894716ad180715acc1
Anna Sibova <anna_sibova@ukr.net>
2023-04-15 22:12:02 +0300

    fix friendlist

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={css.status}>{isOnline}</span>
+      <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
